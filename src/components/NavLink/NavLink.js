7cd87d19fc8357fe365b05ca6423aa20c4b7e094
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-05 22:29:42 +0100

    added RouterNavLink to NavLink

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { NavLink as RouterNavLink } from 'react-router-dom'
+
 import classes from './styles.module.css'
 
 export const NavLink = (props) => {
   const {
     className,
+    children,
+    to,
     ...otherProps
   } = props
 
   return (
-    <div
+    <li
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-
-    </div>
+      <RouterNavLink
+        to={to}
+      >
+        {children}
+      </RouterNavLink>
+    </li>
   )
 }
 
 NavLink.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.node,
+  to: PropTypes.string.isRequired
 }
 
 export default NavLink

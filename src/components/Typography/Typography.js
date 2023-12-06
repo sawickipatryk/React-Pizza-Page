5efd1a851115d4fa8785080b31dd09754088bd7a
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-06 11:50:19 +0100

    Typography style done

--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -5,22 +5,29 @@ import classes from './styles.module.css'
 
 export const Typography = (props) => {
   const {
+    children,
     className,
+    variant,
     ...otherProps
   } = props
 
+  const variantClasses = classes[variant]
+
   return (
-    <div
-      className={`${classes.root}${className ? ` ${className}` : ''}`}
+    <span
+      className={`${classes.root}${className ? ` ${className}` : ''}${variantClasses ? ` ${variantClasses}` : ''}`}
       {...otherProps}
     >
-
-    </div>
+      {children}
+    </span>
   )
 }
 
 Typography.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.node,
+  variant: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'text', 'button'])
+
 }
 
 export default Typography

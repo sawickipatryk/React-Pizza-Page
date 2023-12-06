db08bc048798d156e2625740f36732d9dda9db94
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-06 11:55:13 +0100

    Added Button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,21 +6,27 @@ import classes from './styles.module.css'
 export const Button = (props) => {
   const {
     className,
+    children,
+    variant,
     ...otherProps
   } = props
 
+  const variantClasses = classes[variant]
+
   return (
-    <div
-      className={`${classes.root}${className ? ` ${className}` : ''}`}
+    <button
+      className={`${classes.root}${className ? ` ${className}` : ''}${variantClasses ? ` ${variantClasses}` : ''}`}
       {...otherProps}
     >
-
-    </div>
+      {children}
+    </button>
   )
 }
 
 Button.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.node,
+  variant: PropTypes.oneOf(['contained', 'toogle'])
 }
 
 export default Button

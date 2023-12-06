21518e86f9d4526cbb70da19b06990079eb31a8d
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-06 12:01:33 +0100

    Added ToggleMenu SVG

--- src/components/ToggleMenu/ToggleMenu.js
+++ src/components/ToggleMenu/ToggleMenu.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 
 import classes from './styles.module.css'
 
+import Button from '../Button'
+
 export const ToggleMenu = (props) => {
   const {
     className,
@@ -14,6 +16,22 @@ export const ToggleMenu = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
+      <Button
+        variant={'toggle'}
+      >
+        <svg
+          className={'toggle__icon'}
+          xmlns={'http://www.w3.org/2000/svg'}
+          width={'35'}
+          height={'35'}
+          viewBox={'0 0 20 20'}
+        >
+          <path
+            fill={'currentColor'}
+            d={'M1 3v2h18V3zm0 8h18V9H1zm0 6h18v-2H1z'}
+          />
+        </svg>
+      </Button>
     </div>
   )
 }

506b7d1566b2f3df70f44bd5490e0b9bff8ad95b
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-06 19:09:43 +0100

    FastChoice Mobile Done

--- src/components/FastChoice/FastChoice.js
+++ src/components/FastChoice/FastChoice.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 
 import classes from './styles.module.css'
 
+import Container from '../Container'
+import Typography from '../Typography'
+import Button from '../Button'
+
 export const FastChoice = (props) => {
   const {
     className,
@@ -14,7 +18,50 @@ export const FastChoice = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-
+      <Container
+        className={classes.container}
+      >
+        <div
+          className={classes.leftContainer}
+        >
+          <div
+            className={classes.textContainer}
+          >
+            <Typography
+              className={classes.h3}
+              variant={'h3'}
+            >
+              PEPE BIANCO
+            </Typography>
+            <Button
+              className={classes.button}
+              variant={'contained'}
+            >
+              ORDER NOW
+            </Button>
+          </div>
+        </div>
+        <div
+          className={classes.rightContainer}
+        >
+          <div
+            className={classes.textContainer}
+          >
+            <Typography
+              className={classes.h3}
+              variant={'h3'}
+            >
+              PEPE BIANCO
+            </Typography>
+            <Button
+              className={classes.button}
+              variant={'contained'}
+            >
+              ORDER NOW
+            </Button>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }

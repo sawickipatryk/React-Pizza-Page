6d744c2dbc9e1f0ccd25caf9121e236dad2cf67c
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-08 15:17:12 +0100

    Gallery Component Done

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types'
 
 import classes from './styles.module.css'
 
+import img from './gallery.jpg'
+import img1 from './gallery1.jpg'
+
+import Container from '../Container'
+
 export const Gallery = (props) => {
   const {
     className,
@@ -14,7 +19,64 @@ export const Gallery = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-
+      <Container
+        className={classes.container}
+      >
+        <div
+          className={classes.imgContainer}
+        >
+          <img
+            className={classes.imgContainer}
+            src={img}
+            alt={'from restuarant inside'}
+          />
+        </div>
+        <div
+          className={classes.imgContainer}
+        >
+          <img
+            className={classes.imgContainer}
+            src={img1}
+            alt={'from restuarant inside'}
+          />
+        </div>
+        <div
+          className={classes.imgContainer}
+        >
+          <img
+            className={classes.imgContainer}
+            src={img}
+            alt={'from restuarant inside'}
+          />
+        </div>
+        <div
+          className={classes.imgContainer}
+        >
+          <img
+            className={classes.imgContainer}
+            src={img1}
+            alt={'from restuarant inside'}
+          />
+        </div>
+        <div
+          className={classes.imgContainer}
+        >
+          <img
+            className={classes.imgContainer}
+            src={img}
+            alt={'from restuarant inside'}
+          />
+        </div>
+        <div
+          className={classes.imgContainer}
+        >
+          <img
+            className={classes.img}
+            src={img1}
+            alt={'from restuarant inside'}
+          />
+        </div>
+      </Container>
     </div>
   )
 }

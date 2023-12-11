6e39939289cf6951211ea2163392fbb8080bf8d4
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-11 21:14:44 +0100

    added auth state and onClickCreateAccount function

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types'
 
 import classes from './styles.module.css'
 
+import { createActionSetInfo } from '../../state/loaders'
+import { useDispatch } from 'react-redux'
+
 import { useForm, FormProvider } from 'react-hook-form'
 
 import AuthLayout from '../../layouts/AuthLayout'
 import SignUpForm from '../../components/SignUpForm'
+
+import { signUp, getUserData } from '../../auth'
+
+import handleAsyncAction from '../../handleAsyncAction'
 
 export const SignUpPage = (props) => {
   const {
@@ -15,17 +22,16 @@ export const SignUpPage = (props) => {
   } = props
 
   const methods = useForm()
-  const { handleSubmit, reset } = methods
+  const { handleSubmit } = methods
+  const dispatch = useDispatch()
 
-  const onSubmit = handleSubmit(
-    (data, e) => {
-      console.log('valid', data)
-      reset()
-    },
-    (errors, e) => {
-      console.log('Error', errors)
-    }
-  )
+  const onClickCreateAccount = React.useCallback(async (email, password) => {
+    handleAsyncAction(async () => {
+      await signUp(email, password)
+      dispatch(createActionSetInfo('User account created. User is logged in!'))
+      await getUserData()
+    })
+  }, [dispatch])
 
   return (
     <div
@@ -39,7 +45,7 @@ export const SignUpPage = (props) => {
             {...methods}
           >
             <SignUpForm
-              onSubmit={onSubmit}
+              onSubmit={handleSubmit((data) => onClickCreateAccount(data.email, data.password))}
             />
           </FormProvider>
       }

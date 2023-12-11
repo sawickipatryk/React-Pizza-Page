4c120dfcc53a447a89f3d6048e9e45795e640886
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-11 21:12:17 +0100

    deleted export decodeToken

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,7 +1,6 @@
 export * from './signIn'
 export * from './signUp'
 export * from './token'
-export * from './decodeToken'
 export * from './getUserData'
 export * from './updateUser'
 export * from './sendPasswordResetEmail'

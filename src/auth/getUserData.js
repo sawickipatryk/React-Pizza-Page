70402e5db1f846bea57e57e96a3c73ac9f750045
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-12 16:51:54 +0100

    Added store and dispatch action to files

--- src/auth/getUserData.js
+++ src/auth/getUserData.js
@@ -2,6 +2,13 @@ import { makeRequest } from './makeRequest'
 import { getIdToken } from './token'
 import { FIREBASE_APP_KEY } from './const'
 
+import { store } from '../store'
+
+import {
+  createActionSetIsUserLoggedId,
+  createActionSetUserEmail
+} from '../state/auth'
+
 const GET_USER_DATA_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=' + FIREBASE_APP_KEY
 
 export const getUserData = () => {
@@ -17,7 +24,13 @@ export const getUserData = () => {
         idToken: token
       })
     }
-  ).then((data) => data && data.users && data.users[0])
+  ).then((data) => {
+    const user = data && data.users && data.users[0]
+    store.dispatch(createActionSetIsUserLoggedId())
+    store.dispatch(createActionSetUserEmail(user.email))
+    return data && data.users && data.users[0]
+  }
+  )
 }
 
 export default getUserData
--- src/auth/logOut.js
+++ src/auth/logOut.js
@@ -1,8 +1,17 @@
 import { removeIdToken, removeRefreshToken } from './token'
 
+import { store } from '../store'
+
+import {
+  createActionRemoveIsUserLoggedId,
+  createActionRemoveUserEmail
+} from '../state/auth'
+
 export const logOut = () => {
   removeIdToken()
   removeRefreshToken()
+  store.dispatch(createActionRemoveIsUserLoggedId())
+  store.dispatch(createActionRemoveUserEmail())
   return Promise.resolve()
 }
 

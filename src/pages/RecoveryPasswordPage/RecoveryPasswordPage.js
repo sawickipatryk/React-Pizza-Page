982edbde7498ba5ca8e220b88bd1ac4aa3a847bf
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-17 13:27:36 +0100

    recoverypasswordpage work

--- src/pages/RecoveryPasswordPage/RecoveryPasswordPage.js
+++ src/pages/RecoveryPasswordPage/RecoveryPasswordPage.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types'
 
 import classes from './styles.module.css'
 
+import { createActionSetInfo } from '../../state/loaders'
+
+import { useDispatch } from 'react-redux'
+
 import { useForm, FormProvider } from 'react-hook-form'
+
+import { sendPasswordResetEmail, getUserData } from '../../auth'
 
 import AuthLayout from '../../layouts/AuthLayout'
 import RecoveryPasswordForm from '../../components/RecoveryPasswordForm'
+import handleAsyncAction from '../../handleAsyncAction'
 
 export const RecoveryPasswordPage = (props) => {
   const {
@@ -15,15 +22,16 @@ export const RecoveryPasswordPage = (props) => {
   } = props
 
   const methods = useForm()
-  const { handleSubmit, reset } = methods
+  const { handleSubmit } = methods
+  const dispatch = useDispatch()
 
-  const onSubmit = handleSubmit(
-    (data, e) => {
-      reset()
-    },
-    (errors, e) => {
-    }
-  )
+  const onClickLogin = React.useCallback(async (email) => {
+    handleAsyncAction(async () => {
+      await sendPasswordResetEmail(email)
+      dispatch(createActionSetInfo('Check your email box'))
+      await getUserData()
+    })
+  }, [dispatch])
 
   return (
     <div
@@ -37,7 +45,7 @@ export const RecoveryPasswordPage = (props) => {
             {...methods}
           >
             <RecoveryPasswordForm
-              onSubmit={onSubmit}
+              onSubmit={handleSubmit((data) => onClickLogin(data.email))}
             />
           </FormProvider>
       }
